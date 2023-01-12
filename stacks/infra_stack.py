4c95dbb307d83403d1d80769f08769fd1fb4012a
Razvan-Alexandru Nicu <rnicu@adobe.com>
2023-01-13 00:00:50 +0200

    Changed Roles into Groups. Created Email sednind logic

--- stacks/infra_stack.py
+++ stacks/infra_stack.py
@@ -45,11 +45,41 @@ class InfraStack(Stack):
             'MyLambdaFunction',
             runtime=_lambda.Runtime.PYTHON_3_8,
             code=_lambda.Code.from_inline(
-                """
-                def handler(event, context):
-                    print("Event:", event)
-                    return {"message": "Successfully wrote event to the console"}
-                """
+"""
+import boto3
+
+def handler(event, context):
+    # Create an SES client
+    ses = boto3.client('ses')
+
+    event = event['Records'][0]
+    if event['eventName'] == "INSERT":
+        date = event['dynamodb']['Keys']['date']['S']
+        message = event['dynamodb']['Keys']['announcement']['S']
+        print(f"Date: {date}. Announcement: {message}")
+
+        # Define the email message
+        message = {
+            'Subject': {
+                'Data': 'New Announcement Published',
+                'Charset': 'UTF-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': f'Date: {date}. Announcement: {message}',
+                    'Charset': 'UTF-8'
+                }
+            }
+        }
+
+        # Send the email
+        ses.send_email(
+            Source='posd.announcements.aws@gmail.com',
+            Destination={'ToAddresses': ['razvanalexandrunicu@gmail.com']},
+            Message=message
+        )
+    return {"message": "Successfully handled event"}
+"""
                 ),
             handler='index.handler'
             )
@@ -58,31 +88,57 @@ class InfraStack(Stack):
         announcements_table.grant_read_data(sns_publisher_lambda)
         sns_publisher_lambda.add_event_source(dynamo_event.DynamoEventSource(announcements_table, starting_position=_lambda.StartingPosition.TRIM_HORIZON, batch_size=5, bisect_batch_on_error=True,retry_attempts=10))
 
-        # Create IAM Roles used by the employees
+        # Create IAM Groups used by the employees
         # (User)Admin, Engineer, Support Engineer, HumanResources
-        engineer_role = iam.Role(self, "EngineerRole", role_name="EngineerRole", assumed_by=iam.AccountPrincipal('593177338706'))
-        support_engineer_role = iam.Role(self, "SupportEngineerRole", role_name="SupportEngineerRole", assumed_by=iam.ArnPrincipal('593177338706'))
-        human_resources_role = iam.Role(self, "HumanResourcesRole", role_name="HumanResourcesRole", assumed_by=iam.ArnPrincipal('593177338706'))
+        engineering_group = iam.Group(self, "EngineeringDepartment", group_name="EngineeringDepartment")
+        support_engineering_group = iam.Group(self, "SupportEngineerRole", group_name="SupportEngineeringDepartment")
+        human_resources_group = iam.Group(self, "HumanResourcesRole", group_name="HumanResourcesDepartment")
 
 
 
-        # Attach S3 permissions to roles for the approved_artifacts_bucket bucket
-        support_engineer_role.add_to_policy(
-            iam.PolicyStatement(
-                actions=["s3:PutObject", "s3:GetObject", "s3:ListBucket"],
+        # Attach S3 permissions to groups for the approved_artifacts_bucket bucket
+        
+        # Policies for read-only
+        bucket_read_policy = iam.PolicyStatement(
+                actions=["s3:Get*", "s3:List*"],
                 resources=[approved_artifacts_bucket.bucket_arn]
             )
-        )
-        engineer_role.add_to_policy(
-            iam.PolicyStatement(
-                actions=["s3:GetObject", "s3:ListBucket"],
+        bucket_objects_read_policy = iam.PolicyStatement(
+                actions=["s3:Get*"],
+                resources=[approved_artifacts_bucket.arn_for_objects('*')]
+            )
+
+        # Policies for read-write
+        bucket_read_write_policy = iam.PolicyStatement(
+                actions=["s3:*"],
                 resources=[approved_artifacts_bucket.bucket_arn]
             )
-        )
+        bucket_objects_read_write_policy = iam.PolicyStatement(
+                actions=["s3:*", ],
+                resources=[approved_artifacts_bucket.arn_for_objects('*')]
+            )
 
-        # Attach DynamoDB permissions to roles
-        announcements_table.grant_read_write_data(human_resources_role)
-        announcements_table.grant_read_data(engineer_role)
-        announcements_table.grant_read_data(support_engineer_role)
+        # Permissions for Engineering department
+        engineering_group.add_to_policy(bucket_read_policy)
+        engineering_group.add_to_policy(bucket_objects_read_policy)
+
+        # Permissions for Support Engineering department
+        support_engineering_group.add_to_policy(bucket_read_write_policy)
+        support_engineering_group.add_to_policy(bucket_objects_read_write_policy)
+
+        # Attach DynamoDB permissions to groups
+        announcements_table.grant_read_write_data(human_resources_group)
+        announcements_table.grant_read_data(engineering_group)
+        announcements_table.grant_read_data(support_engineering_group)
+
+        # Allow lambda to send email with SES
+
+        # Create the IAM policy for SES
+        policy = iam.PolicyStatement(
+            actions=["ses:SendEmail", "ses:SendRawEmail"],
+            resources=["*"],
+        )
+        sns_publisher_lambda.add_to_role_policy(policy)
+
 
 
